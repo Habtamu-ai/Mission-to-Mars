3c7cd4850eff4e47abbbd33b9c2785ff8729e8fd
Habtamu Molla <78656720+Habtamu-ai@users.noreply.github.com>
2021-04-22 15:00:50 -0400

    Adding the flask app file.

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, redirect, url_for
 from flask_pymongo import PyMongo
-import scraping
+import Scraping
 app = Flask(__name__)
 # Use flask_pymongo to set up mongo connection
 app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
